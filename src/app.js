0231805e43d31eefce8595fe13f440998cf2e3c2
Jackie Liu <jinke.liu@autodesk.com>
2019-08-22 13:42:02 +0800

    Initial version

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const tuc = require("temp-units-conv");
+let response;
+
+const scales = {
+  c: "celsius",
+  f: "fahrenheit",
+  k: "kelvin"
+};
+
+exports.handler = async event => {
+  let conversion = event.pathParameters.conversion;
+  let originalValue = event.pathParameters.value;
+
+  console.log({ conversion, originalValue });
+
+  let answer = tuc[conversion](originalValue);
+  try {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        source: scales[conversion[0]],
+        target: scales[conversion[2]],
+        original: originalValue,
+        answer: answer
+      })
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+
+  return response;
+};
